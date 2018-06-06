30112187572a4c55c7c3e16533b8d301d2ffa351
John Riccardi <riccjohn@gmail.com>
2018-06-06 14:02:56 -0500

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,59 @@
+const landingSites = [
+  'Junk Junction',
+  'Haunted Hills',
+  'Pleasant Park',
+  'North Pleasant',
+  'Motel',
+  'Anarchy Acres',
+  'Loot Lake House',
+  'Loot Lake Factories',
+  'Tomato Town',
+  'Risky Reels',
+  'Wailing Woods',
+  'Edit House',
+  'Lonely Lodge',
+  'Race Track',
+  'Retail Row',
+  'Dusty Divot Building',
+  'Dusty Divot Crater',
+  'Salty Springs',
+  'Lucky Landing',
+  'Fatal Fields',
+  'Prison',
+  'Moisty Mire',
+  'Flush Factory',
+  'South Factories',
+  'Greasy Grove',
+  'Shifty Shafts',
+  'Indoor Soccer Field',
+  'Snobby Shores',
+  'Missile Silo',
+  'Tilted Towers',
+  'Haunted House',
+  'Super Hideout',
+  'Loot Lake North',
+]
+
+const whereWeLandingBoys = () => {
+	let answer = '';
+  console.log('\n', '\n');
+  let randNum = Math.floor(Math.random()* (landingSites.length - 1));
+	// let randNum = 6;
+  if (randNum === 6) {
+		return "You're Fucked. Land at " + landingSites[randNum];
+    console.log("You're Fucked. Land at", landingSites[randNum])
+  } else {
+		return landingSites[randNum];
+		console.log('Land at', landingSites[randNum]);
+	}
+  console.log('\n', '\n');
+}
+
+// whereWeLandingBoys();
+
+const result = () => {
+	const landing = whereWeLandingBoys();
+	document.getElementById('results').innerHTML = landing;
+	// document.getElementById('results').innerHTML = '<h3>landing</h3>';
+	document.getElementById('results').style.display = "flex";
+}
